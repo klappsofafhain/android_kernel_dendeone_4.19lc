0cb7df9ec6e4c076f6b0b900b8ad7639f3303146
Andie_Klampfe <andi_schmidt@posteo.de>
2024-12-16 17:17:54 +0100

    bd7710 rear & front flashlight

--- drivers/misc/mediatek/flashlight/flashlight-device.c
+++ drivers/misc/mediatek/flashlight/flashlight-device.c
@@ -106,11 +106,9 @@
 #else
 	const struct flashlight_device_id flashlight_id[] = {
 		/* {TYPE, CT, PART, "NAME", CHANNEL, DECOUPLE} */
-		{0, 0, 0, "flashlights-none", -1, 0},
-		{0, 1, 0, "flashlights-none", -1, 0},
-		{1, 0, 0, "flashlights-none", -1, 0},
-		{1, 1, 0, "flashlights-none", -1, 0},
-		{0, 0, 1, "flashlights-none", -1, 0},
+		{0, 0, 0, "flashlights-bd7710", 0, 0},
+	    {0, 1, 0, "flashlights-bd7710", 1, 0},
+		{1, 0, 0, "flashlights-bd7710_front", 2, 0},
 		{0, 1, 1, "flashlights-none", -1, 0},
 		{1, 0, 1, "flashlights-none", -1, 0},
 		{1, 1, 1, "flashlights-none", -1, 0},
--- drivers/misc/mediatek/flashlight/flashlight-dt.h
+++ drivers/misc/mediatek/flashlight/flashlight-dt.h
@@ -26,5 +26,11 @@
 #define OCP81373_DTNAME     "mediatek,flashlights_ocp81373"
 #define FLMAIN_DTNAME     "mediatek,flashlights_main_gpio"
 #define FLSUB_DTNAME      "mediatek,flashlights_sub_gpio"
+/*aquaris a4_5/m4_5 flashlights*/
+#define BD7710_DTNAME     "mediatek,flashlights_bd7710"
+#define BD7710_DTNAME_I2C "mediatek,bd7710"
+#define BD7710_FRONT_DTNAME "mediatek,flashlights_bd7710_front"
+#define BD7710_FRONT_DTNAME_I2C "mediatek,bd7710"
+/*aquaris a4_5/m4_5 flashlights*/
 
 #endif /* _FLASHLIGHT_DT_H */
